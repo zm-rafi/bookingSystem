066f2a8a7fa7be6112a59f913a1692b12d91c8dc
Your Namezm-rafi <zrafi221217@bscse.uiu.ac.bd>
2025-06-26 23:45:47 +0600

    chatbot

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Header from '../components/Header'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
-
+import Chatbot from './chatbot'
 const Home = () => {
   return (
     <div>
       <Header />
+      <Chatbot/>
       <TopDoctors />
       <Banner />
     </div>
